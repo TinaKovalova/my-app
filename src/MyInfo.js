f632163a8743b1b2f152471ae31211c0a90e2eca
WIN-GBFPBN2U5AM\User <kovalevatkv@gmail.com>
2021-03-21 02:11:32 +0200

    finish

---
+++ src/MyInfo.js
@@ -0,0 +1,14 @@
+const MyInfo = () =>
+(<div className="myInfo">
+    <h1>Тина</h1>
+    <p>Бухгалтер...</p>
+    <p>... желающий стать программистом</p>
+    <div className="places">
+        <ul>
+            <li>Донецк</li>
+            <li>Италия</li>
+            <li>Грузия</li>
+        </ul>
+    </div>
+</div>)
+export default MyInfo
